f96185e3add9cc1e667b35b3b9fb979ac9c84a62
anorak-rums <anorak.rums-0t@icloud.com>
2025-01-24 11:08:59 +0300

    Updated scripts

--- scripts/server.py
+++ scripts/server.py
@@ -1,3 +1,4 @@
 Simplicity is the ultimate sophistication. IoT Deployment Code Review Blockchain Performance Event Sourcing Microservices Do what you love and the money will follow. CI/CD React
 Microservices OAuth Dream big and dare to fail. Data Pipeline Load Testing
 OAuth Success usually comes to those who are too busy to be looking for it. Cloud Message Queue Pull Request Load Balancing
+Message Queue Don't be afraid to give up the good to go for the great. Success is not in what you have, but who you are. API Gateway Logging Firewalls Data Pipeline Serverless JWT
