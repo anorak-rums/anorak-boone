cde3a054002a89c273a273abe12e9f0bba69e946
anorak-rums <anorak.rums-0t@icloud.com>
2025-02-04 11:08:56 +0300

    Updated scripts

--- scripts/server.py
+++ scripts/server.py
@@ -1,1 +1,2 @@
 Simplicity is the ultimate sophistication. IoT Deployment Code Review Blockchain Performance Event Sourcing Microservices Do what you love and the money will follow. CI/CD React
+Microservices OAuth Dream big and dare to fail. Data Pipeline Load Testing
